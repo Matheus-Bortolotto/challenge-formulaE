5ad070f05576bce2a60fba53a52fe21dd434a771
Matheus Bortolotto <matheusbortolol@hotmail.com>
2024-05-09 14:53:59 -0300

    adicionando a tabela de classificação funcionando com javascript

---
+++ js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var tabela = document.getElementById("tabela-classificacao");
+  var corpoTabela = document.getElementById("corpo-tabela");
+  var mostrarMaisBtn = document.getElementById("mostrar-mais");
+  var mostrarMenosBtn = document.getElementById("mostrar-menos");
+
+  var linhasPorPagina = 10;
+  var linhasExibidas = linhasPorPagina;
+
+  function mostrarProximasLinhas() {
+    for (var i = linhasExibidas; i < linhasExibidas + linhasPorPagina; i++) {
+      if (corpoTabela.rows[i]) {
+        corpoTabela.rows[i].style.display = "";
+      }
+    }
+    linhasExibidas += linhasPorPagina;
+
+    if (linhasExibidas >= corpoTabela.rows.length) {
+      mostrarMaisBtn.style.display = "none";
+    }
+  }
+
+  function mostrarMenosLinhas() {
+    for (
+      var i = linhasExibidas - 1;
+      i >= linhasExibidas - linhasPorPagina;
+      i--
+    ) {
+      if (corpoTabela.rows[i]) {
+        corpoTabela.rows[i].style.display = "none";
+      }
+    }
+    linhasExibidas -= linhasPorPagina;
+
+    mostrarMaisBtn.style.display = "";
+  }
+
+  for (var i = linhasPorPagina; i < corpoTabela.rows.length; i++) {
+    corpoTabela.rows[i].style.display = "none";
+  }
+
+  mostrarMaisBtn.addEventListener("click", mostrarProximasLinhas);
+
+  mostrarMenosBtn.addEventListener("click", mostrarMenosLinhas);
+});
